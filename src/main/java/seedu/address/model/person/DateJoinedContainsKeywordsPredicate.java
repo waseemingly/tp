254615d7d6a487d3722209c5e87bf254522b75f5
waseem <mohammedwaseem1000@gmail.com>
2023-10-17 23:14:41 +0800

    Add functionality where every user can find for contacts related to the any keyword attributing to employees

--- src/main/java/seedu/address/model/person/DateJoinedContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/DateJoinedContainsKeywordsPredicate.java
@@ -6,19 +6,19 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
- * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code Person}'s {@code DateJoined} matches any of the keywords given.
  */
-public class NameContainsKeywordsPredicate implements KeywordPredicate<Person> {
+public class DateJoinedContainsKeywordsPredicate implements KeywordPredicate<Person> {
     private final List<String> keywords;
 
-    public NameContainsKeywordsPredicate(List<String> keywords) {
+    public DateJoinedContainsKeywordsPredicate(List<String> keywords) {
         this.keywords = keywords;
     }
 
     @Override
     public boolean test(Person person) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getDateJoined().toString(), keyword));
     }
 
     @Override
@@ -28,12 +28,12 @@ public class NameContainsKeywordsPredicate implements KeywordPredicate<Person> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof NameContainsKeywordsPredicate)) {
+        if (!(other instanceof DateJoinedContainsKeywordsPredicate)) {
             return false;
         }
 
-        NameContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (NameContainsKeywordsPredicate) other;
-        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
+        DateJoinedContainsKeywordsPredicate otherDateJoinedContainsKeywordsPredicate = (DateJoinedContainsKeywordsPredicate) other;
+        return keywords.equals(otherDateJoinedContainsKeywordsPredicate.keywords);
     }
 
     @Override
--- src/main/java/seedu/address/model/person/EmailContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/EmailContainsKeywordsPredicate.java
@@ -6,19 +6,19 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
- * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code Person}'s {@code Email} matches any of the keywords given.
  */
-public class NameContainsKeywordsPredicate implements KeywordPredicate<Person> {
+public class EmailContainsKeywordsPredicate implements KeywordPredicate<Person> {
     private final List<String> keywords;
 
-    public NameContainsKeywordsPredicate(List<String> keywords) {
+    public EmailContainsKeywordsPredicate(List<String> keywords) {
         this.keywords = keywords;
     }
 
     @Override
     public boolean test(Person person) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getEmail().value, keyword));
     }
 
     @Override
@@ -28,12 +28,12 @@ public class NameContainsKeywordsPredicate implements KeywordPredicate<Person> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof NameContainsKeywordsPredicate)) {
+        if (!(other instanceof EmailContainsKeywordsPredicate)) {
             return false;
         }
 
-        NameContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (NameContainsKeywordsPredicate) other;
-        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
+        EmailContainsKeywordsPredicate otherEmailContainsKeywordsPredicate = (EmailContainsKeywordsPredicate) other;
+        return keywords.equals(otherEmailContainsKeywordsPredicate.keywords);
     }
 
     @Override
