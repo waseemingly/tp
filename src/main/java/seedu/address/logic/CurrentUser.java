2e40c03bfece1602c2b721bad3e79938e4aeee1a
emzm2023 <meeselevels@elizabeths-MacBook-Air-3.local>
2023-10-18 21:45:27 +0800

    Combine CurrentUser and DummyCurrentUser class to CurrentUser class

--- src/main/java/seedu/address/logic/CurrentUser.java
+++ src/main/java/seedu/address/logic/CurrentUser.java
@@ -1,7 +1,9 @@
 package seedu.address.logic;
 
-import seedu.address.logic.parser.AddressBookParser;
+import seedu.address.logic.commands.EditSelfCommand;
+import seedu.address.logic.parser.EditCommandParser;
 import seedu.address.model.person.Person;
+import seedu.address.logic.parser.AddressBookParser;
 import seedu.address.ui.PersonCard;
 
 /**
@@ -11,16 +13,18 @@ import seedu.address.ui.PersonCard;
  */
 
 public class CurrentUser {
-
     private final Person user;
 
-    /**
-     * Constructs a CurrentUser object with the specified user.
-     *
-     * @param user The Person object representing the current user.
+    /** 
+     * Constructs a CurrentUser object with the specified user. Calls class methods of relevant functions to preset
+     * the expected behaviours.
+     * 
+     * @param user The current user logged in.
      */
-    public CurrentUser(Person user) {
+    CurrentUser(Person user) {
         this.user = user;
+        EditCommandParser.setCurrentUserRole(user.getRole());
+        EditSelfCommand.setCurrentUser(user);
         AddressBookParser.setCurrentUser(user);
         PersonCard.setCurrentUser(user);
     }
