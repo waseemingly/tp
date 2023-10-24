b966773c402cf513822977312d77cc8ad15bab98
Mahidharah <mahidharah@gmail.com>
2023-10-25 03:29:58 +0800

    Revert "Edited SampleData to include only developers and clients"
    This reverts commit 483c18b46257120c7c5724236bb1e333bc3df04c.

--- src/main/java/seedu/address/model/person/DateJoinedContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/DateJoinedContainsKeywordsPredicate.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.model.developer.Developer;
 
 /**
  * Tests that a {@code Person}'s {@code DateJoined} matches any of the keywords given.
  */
-public class DateJoinedContainsKeywordsPredicate implements KeywordPredicate<Developer> {
+public class DateJoinedContainsKeywordsPredicate implements KeywordPredicate<Person> {
     private final List<String> keywords;
 
     public DateJoinedContainsKeywordsPredicate(List<String> keywords) {
@@ -17,9 +16,9 @@ public class DateJoinedContainsKeywordsPredicate implements KeywordPredicate<Dev
     }
 
     @Override
-    public boolean test(Developer developer) {
+    public boolean test(Person person) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(developer.getDateJoined().toString(), keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getDateJoined().toString(), keyword));
     }
 
     @Override
--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -5,14 +5,9 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
-import seedu.address.model.Client.Client;
-import seedu.address.model.Client.Document;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.commons.Date;
 import seedu.address.model.commons.Name;
-import seedu.address.model.developer.Developer;
-import seedu.address.model.developer.GithubId;
-import seedu.address.model.developer.Rating;
 import seedu.address.model.person.Role;
 import seedu.address.model.developer.Salary;
 import seedu.address.model.person.*;
@@ -24,18 +19,18 @@ import seedu.address.model.project.Project;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Developer(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), new Role("Dev"), getProjectSet(), new Salary("5000"), new Date("15-12-2019"), new GithubId("mahidharah"), new Rating("5.0")),
-            new Developer(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Role("Dev"), getProjectSet("AndroidApp", "customWebsite"), new Salary("6000"), new Date("16-11-2020"), new GithubId("mahidharah1"), new Rating("5.0")),
-            new Developer(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Role("Developer"), getProjectSet("AndroidApp"), new Salary("4500"), new Date("20-10-2020"), new GithubId("mahidharah2"), new Rating("5.0")),
-            new Developer(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Role("Developer"), getProjectSet("customWebsite"), new Salary("5500"), new Date("23-09-2021"), new GithubId("mahidharah3"), new Rating("5.0")),
-            new Client(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"), new Role("HR"), getProjectSet(), new Name("Google"), new Document("https://www.google.com/")),
-            new Client(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), new Role("Manager"), getProjectSet("AndroidApp", "customWebsite"), new Name("Google1"), new Document("https://www.google.com/"))
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                new Address("Blk 30 Geylang Street 29, #06-40"), new Date("15-12-2019"), new Role("HR"), new Salary("5000"), getProjectSet()),
+            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Date("16-11-2020"), new Role("Manager"), new Salary("6000"), getProjectSet("AndroidApp", "customWebsite")),
+            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Date("20-10-2020"),  new Role("Developer"), new Salary("4500"), getProjectSet("AndroidApp")),
+            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Date("23-09-2021"), new Role("Developer"), new Salary("5500"), getProjectSet("customWebsite")),
+            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+                new Address("Blk 47 Tampines Street 20, #17-35"), new Date("24-12-2020"), new Role("HR"), new Salary("5000"), getProjectSet()),
+            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+                new Address("Blk 45 Aljunied Street 85, #11-31"), new Date("29-02-2022"), new Role("Developer"), new Salary("5500"), getProjectSet("AndroidApp", "customWebsite"))
         };
     }
 
