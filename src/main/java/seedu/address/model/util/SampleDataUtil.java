4ce1ba8e470f7d65ec596360158904998920da54
Mahidharah <mahidharah@gmail.com>
2023-11-02 17:36:50 +0800

    Add Sample Data
    Sample data for clients and projects added with inter-dependancies for testing

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -30,41 +30,55 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE;
  */
 public class SampleDataUtil {
     public static Developer[] getSampleDevelopers() {
+        Set<String> projectSet1 = getProjectSet("CodeContact", "TeamTrekker");
+        Set<String> projectSet2 = getProjectSet("Orbital", "Appollo");
+        Set<String> projectSet3 = getProjectSet("CodeContact");
+        Set<String> projectSet4 = getProjectSet("TeamTrekker", "Appollo");
+
         return new Developer[] {
-            new seedu.address.model.developer.Developer(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), new Role("Developer"), getProjectSet(), new Salary("5000"), new Date("15-12-2019"), new GithubId("mahidharah"), new Rating("5.0")),
-            new seedu.address.model.developer.Developer(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Role("Developer"), getProjectSet("AndroidApp", "customWebsite"), new Salary("6000"), new Date("16-11-2020"), new GithubId("mahidharah1"), new Rating("5.0")),
-            new seedu.address.model.developer.Developer(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Role("Developer"), getProjectSet("AndroidApp"), new Salary("4500"), new Date("20-10-2020"), new GithubId("mahidharah2"), new Rating("5.0")),
-            new Developer(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Role("Developer"), getProjectSet("customWebsite"), new Salary("5500"), new Date("23-09-2021"), new GithubId("mahidharah3"), new Rating("5.0")),
-                  };
+                new Developer(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                        new Address("Blk 30 Geylang Street 29, #06-40"), new Role("Developer"), projectSet1,
+                        new Salary("5000"), new Date("15-12-2019"), new GithubId("mahidharah"), new Rating("5.0")),
+                new Developer(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Role("Developer"), projectSet2,
+                        new Salary("6000"), new Date("16-11-2020"), new GithubId("mahidharah1"), new Rating("5.0")),
+                new Developer(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Role("Developer"), projectSet3,
+                        new Salary("4500"), new Date("20-10-2020"), new GithubId("mahidharah2"), new Rating("5.0")),
+                new Developer(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Role("Developer"), projectSet4,
+                        new Salary("5500"), new Date("23-09-2021"), new GithubId("mahidharah3"), new Rating("5.0"))
+        };
     }
 
     public static Client[] getSampleClients() {
+        Set<String> projectSet1 = getProjectSet("CodeContact", "TeamTrekker");
+        Set<String> projectSet2 = getProjectSet("Orbital", "Appollo");
+        Set<String> projectSet3 = getProjectSet("CodeContact");
+        Set<String> projectSet4 = getProjectSet("TeamTrekker", "Appollo");
+
         return new Client[] {
-                new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                        new Address("Blk 30 Geylang Street 29, #06-40"), new Role("Developer"),
-                        getProjectSet(), new Name("Google"), new Document("https://www.google.com/")),
-                new Client(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Role("Developer"),
-                        getProjectSet("AndroidApp", "customWebsite"), new Name("Google"), new Document("https://www.google.com/")),
-                new Client(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Role("Developer"),
-                        getProjectSet("AndroidApp"), new Name("Google"), new Document("https://www.google.com/")),
-                new Client(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Role("Developer"),
-                        getProjectSet("customWebsite"), new Name("Google"), new Document("https://www.google.com/")),
+                new Client(new Name("Eva Tang"), new Phone("98765432"), new Email("eva@example.com"),
+                        new Address("Blk 123 Bukit Batok Street 11, #01-01"), new Role("HR"), projectSet1,
+                        new Name("XYZ Corp"), new Document("https://www.xyzcorp.com/")),
+                new Client(new Name("Fiona Goh"), new Phone("87654321"), new Email("fiona@example.com"),
+                        new Address("Blk 456 Jurong East Street 33, #02-02"), new Role("Manager"), projectSet2,
+                        new Name("ABC Pte Ltd"), new Document("https://www.abc.com/")),
+                new Client(new Name("George Lim"), new Phone("76543210"), new Email("george@example.com"),
+                        new Address("Blk 789 Woodlands Ave 6, #03-03"), new Role("Developer"), projectSet3,
+                        new Name("MNO Company"), new Document("https://www.mno.com/")),
+                new Client(new Name("Helen Tan"), new Phone("65432109"), new Email("helen@example.com"),
+                        new Address("Blk 321 Tampines Street 33, #04-04"), new Role("HR"), projectSet4,
+                        new Name("PQR LLC"), new Document("https://www.pqr.com/"))
         };
     }
 
     public static Project[] getSampleProjects() {
         return new Project[] {
-                new Project(new Name("CodeContact"), new Description("test"), getDeadlineList("")),
-                new Project(new Name("TeamTrekker"), new Description("test"), getDeadlineList("")),
-                new Project(new Name("Oribital"), new Description("test"), getDeadlineList("")),
-                new Project(new Name("Appollo"), new Description("test"), getDeadlineList("")),
+                new Project(new Name("CodeContact"), new Description("A contact management system"), getDeadlineList("13-10-2021,Phase 1,HIGH,0")),
+                new Project(new Name("TeamTrekker"), new Description("A team collaboration tool"), getDeadlineList("13-11-2021,Phase 2,MEDIUM,0")),
+                new Project(new Name("Orbital"), new Description("A satellite tracking system"), getDeadlineList("13-12-2021,Phase 3,LOW,0")),
+                new Project(new Name("Appollo"), new Description("A mobile app development platform"), getDeadlineList("13-01-2022,Launch,HIGH,0"))
         };
     }
 
@@ -72,6 +86,12 @@ public class SampleDataUtil {
         AddressBook sampleAb = new AddressBook();
         for (Developer sampleDeveloper: getSampleDevelopers()) {
             sampleAb.addDeveloper(sampleDeveloper);
+        }
+        for (Client sampleClient: getSampleClients()) {
+            sampleAb.addClient(sampleClient);
+        }
+        for (Project sampleProject: getSampleProjects()) {
+            sampleAb.addProject(sampleProject);
         }
         return sampleAb;
     }
