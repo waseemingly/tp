38263e0ce8dcb190b2ff3809516db1b87db2f502
MathanC <ncmathan98@gmail.com>
2023-11-02 11:33:08 +0800

    no message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -23,7 +23,7 @@ public class Messages {
     public static final String MESSAGE_INAPPLICABLE_PREFIX_USED = "You tried to edit an inapplicable field! Please check " +
             "the prefixes used and try again. \n%1$s";
     public static final String MESSAGE_NONEXISTENT_PROJECT = "There is no existing Project with the name: %1$s!";
-    public static final String MESSAGE_CLIENTS_LISTED_OVERVIEW = ;
+
 
     public static String getMessageDevelopersListedOverview(int count) {
         return count == 1
