8b19f0de5886b4dabbf3b894f6cec070904f31cd
mingyu <wanmingyu0318@gmail.com>
2023-11-08 16:36:25 +0800

    resolve conflicts

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -14,8 +14,7 @@ public class HelpCommandTest {
 
     @Test
     public void execute_help_success() {
-        CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE,
-                true, false, TabIndex.Developer);
+        CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false, TabIndex.Developer);
         assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
     }
 }
